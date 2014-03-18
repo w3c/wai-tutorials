2bfc30dd7cc3e499a3e9ca356fd34bc0325edc32
Eric Eggert <ee@w3.org>
2014-03-18 14:43:10 -0700

    Minify CSS JS

--- config.rb
+++ config.rb
@@ -146,10 +146,10 @@ activate :directory_indexes
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
