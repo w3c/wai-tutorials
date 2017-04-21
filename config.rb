d4dd0bf88c683efa8e44c5297a8f95d2fed39ec4
Eric Eggert <mail@yatil.net>
2017-04-21 13:14:09 +0200

    deploy options
    (untested)

--- config.rb
+++ config.rb
@@ -219,12 +219,27 @@ configure :build do
   # set :http_prefix, "/Content/images/"
 end
 
-activate :deploy do |deploy|
-  deploy.method = :git
-  # Optional Settings
-  deploy.remote   = 'git@github.com:w3c/wai-tutorials.git' # remote name or git url, default: origin
-  deploy.branch   = 'gh-pages' # default: gh-pages
-  deploy.build_before = true # default: false
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+# config.rb
+case ENV['TARGET'].to_s.downcase
+when 'release'
+  activate :deploy do |deploy|
+    deploy.method = :git
+    # Optional Settings
+    deploy.remote   = 'git@github.com:w3c/wai-tutorials.git' # remote name or git url, default: origin
+    deploy.branch   = 'gh-pages-release-2015-03' # default: gh-pages
+    deploy.build_before = true # default: false
+    # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+    # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  end
+else
+  activate :deploy do |deploy|
+    deploy.method = :git
+    # Optional Settings
+    deploy.remote   = 'git@github.com:w3c/wai-tutorials.git' # remote name or git url, default: origin
+    deploy.branch   = 'gh-pages' # default: gh-pages
+    deploy.build_before = true # default: false
+    # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+    # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  end
 end
+
