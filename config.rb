f9e36b2c0e7aafd347c495f538d8ed06af367aa6
Eric Eggert <mail@yatil.net>
2017-04-23 15:17:43 +0200

    deploy variables

--- config.rb
+++ config.rb
@@ -221,12 +221,12 @@ end
 
 # config.rb
 case ENV['TARGET'].to_s.downcase
-when 'release'
+when 'rendered'
   activate :deploy do |deploy|
     deploy.method = :git
     # Optional Settings
     deploy.remote   = 'git@github.com:w3c/wai-tutorials.git' # remote name or git url, default: origin
-    deploy.branch   = 'gh-pages-release-2015-03' # default: gh-pages
+    deploy.branch   = 'rendered-2015-03' # default: gh-pages
     deploy.build_before = true # default: false
     # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
     # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
@@ -242,4 +242,3 @@ else
     # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
   end
 end
-
